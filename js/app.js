fe8840f9e4910a3c43eb6f53ff62dc1b2efc0157
Bob <lapizjusoh@yahoo.com>
2025-01-03 21:08:53 +0800

    Add Player Switching

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
 // Gameboard, Players, Gameflow
 
-const gameBoard = {}
+function createGameboard() {
+  const gameBoard = {};
+  const gameBoardDisplay = document.querySelector('#game-board')
+
+  for (let i = 0; i < 9; i++) {
+    const div = document.createElement('div');
+    div.setAttribute("class", "grid");
+    gameBoardDisplay.appendChild(div);
+    gameBoard[`grid${i}`] = "null";
+
+    div.addEventListener("click", (e) => {
+
+      if (gameBoard[`grid${i}`] == "null") {
+        e.target.innerHTML = currentPlayer.marker;
+        currentPlayer = (currentPlayer == playerOne ? playerTwo : playerOne);
+        gameBoard[`grid${i}`] = currentPlayer.marker;
+        div.style = "cursor: default;"
+        
+        checkWin(gameBoard);
+      }
+
+    })
+  }
+
+  return gameBoard;
+}
 
 function createPlayer(name, marker, score) {
   const addScore = () => score++;
@@ -9,30 +34,37 @@ function createPlayer(name, marker, score) {
 }
 
 function displayController() {
-
+  createGameboard();
 }
 
-function gamePlay() {
-
+function checkWin(gameBoard) {
+  
+  playerStatus();
 }
 
-function checkWin() {
+function playerStatus() {
+  if (currentPlayer == playerOne) {
+    const playerHUD = document.querySelector(`#player1-hud`);
+    const playerHUDRevert = document.querySelector(`#player2-hud`);
+    playerHUD.setAttribute("style", "background-color:blue;color:white;");
+    playerHUDRevert.setAttribute("style", "background-color:white;color:black;");
 
-}
-
-function gameStart() {
-  const playerOne = createPlayer("Player One", "X", 0);
-  const playerTwo = createPlayer("Player Two", "O", 0);
-
-  const gameBoardDisplay = document.querySelector('#game-board')
-
-  for (let i = 0; i < 9; i++) {
-    const div = document.createElement('div');
-    div.setAttribute("class","grid");
-    gameBoardDisplay.appendChild(div);
-
-    gameBoard[`grid${i}`] = "null";
+    playerHUD.setAttribute("style", "background-color:blue;color:white;");
+  } else if (currentPlayer == playerTwo) {
+    const playerHUD = document.querySelector(`#player2-hud`);
+    const playerHUDRevert = document.querySelector(`#player1-hud`);
+    playerHUD.setAttribute("style", "background-color:red;color:white;");
+    playerHUDRevert.setAttribute("style", "background-color:white;color:black;");
   }
 }
 
-gameStart();  
+function gameStart() {
+  displayController();
+  playerStatus();
+}
+
+const playerOne = createPlayer("Player One", "X", 0);
+const playerTwo = createPlayer("Player Two", "O", 0);
+let currentPlayer = playerOne;
+
+gameStart(); 
