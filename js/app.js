8b10c266e193f2ad5cc7f4cfa3c6d6d8fbc21432
Bob <lapizjusoh@yahoo.com>
2025-01-04 17:45:16 +0800

    Add streamline match three function condition

--- js/app.js
+++ js/app.js
@@ -4,44 +4,34 @@
 6 7 8
 */
 
-const gameBoard = [
-  '','','',
-  '','','',
-  '','',''
-];
 // Initialize gameboard, from index 0 -> 8
-
-const winningCombo = [
-  [0,1,2],[3,4,5],[6,7,8],
-  [0,3,6],[1,4,7],[2,5,8],
-  [0,4,8],[2,4,8]
+const gameBoard = [
+  'X','O','X',
+  'O','X','O',
+  'X','O','X'
 ];
-// Array for storing match three rows(horizontal, vertical and diagonal)
 
-function checkWin() {
+// Initialize match three function
+function checkMatchThree(marker) {
 
-}
+  // check row
+  for (let i = 0; i < gameBoard.length; i+=3) {
+    if ( (gameBoard[i] == marker) && (gameBoard[i + 1] == marker ) && (gameBoard[i + 2] == marker ) ) {
+      return `Player ${marker} wins!`;
+    }
+  }
 
+  // check column
+  for (let i = 0; i < 3; i++) {
+    if ( (gameBoard[i] == 'X') && (gameBoard[i + 3] == 'X' ) && (gameBoard[i + 6] == 'X' ) ) {
+      return `Player ${marker} wins!`;
+    }
+  }
 
-// testing area
-const gameBoardThree = ['X','X','X'];
-
-if ( gameBoard.every((grid) => grid !== '') ) console.log('draw!');
-
-
-
-
-// IF WE BRUTE FORCE IT:-
-
-if (
-  ( (gameBoard[0] == 'X') && (gameBoard[1] == 'X') && (gameBoard[2] == 'X') ) ||
-  ( (gameBoard[3] == 'X') && (gameBoard[4] == 'X') && (gameBoard[5] == 'X') ) ||
-  ( (gameBoard[6] == 'X') && (gameBoard[7] == 'X') && (gameBoard[8] == 'X') ) ||
-  ( (gameBoard[0] == 'X') && (gameBoard[3] == 'X') && (gameBoard[6] == 'X') ) ||
-  ( (gameBoard[1] == 'X') && (gameBoard[4] == 'X') && (gameBoard[7] == 'X') ) ||
-  ( (gameBoard[2] == 'X') && (gameBoard[5] == 'X') && (gameBoard[8] == 'X') ) ||
-  ( (gameBoard[0] == 'X') && (gameBoard[4] == 'X') && (gameBoard[8] == 'X') ) ||
-  ( (gameBoard[2] == 'X') && (gameBoard[4] == 'X') && (gameBoard[8] == 'X') )
-) {
-
+  // check diagonal
+  if ( ( (gameBoard[0] == marker) && (gameBoard[4] == marker) && (gameBoard[8] == marker) ) ||
+  ( (gameBoard[2] == marker) && (gameBoard[4] == marker) && (gameBoard[6] == marker) )
+  ) {
+    return `Player ${marker} wins!`;
+  }
 }
