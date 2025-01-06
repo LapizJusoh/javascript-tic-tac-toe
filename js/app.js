a4174d93c81e4c80192cc1a02802f491bb130652
Bob <lapizjusoh@yahoo.com>
2025-01-07 06:50:49 +0800

    Implement reset after every round

--- js/app.js
+++ js/app.js
@@ -4,55 +4,64 @@
 6 7 8
 */
 
-const gameBoard = ['','','','','','','','',''];
-const playerOne = createPlayer("One", 'X');
-const playerTwo = createPlayer("Two", 'O');
-
+const gameBoard = ['', '', '', '', '', '', '', '', ''];
 function createPlayer(name, marker) {
 
   let score = 0;
   const getScore = () => score;
   const addScore = () => score++;
-  return { name, marker, getScore, addScore}
-}
+  return { name, marker, getScore, addScore }
+};
 
 const displayController = {
   playerScoreDisplay: (player) => document.querySelector(`#player${player.name}-score`),
   playerHUDDisplay: (player) => document.querySelector(`#player${player.name}-hud`),
   playerTurnText: document.querySelector(`#player-turn-text`)
-}
-
-function checkMatchThree(player) {
-
-  // check row
-  for (let i = 0; i < gameBoard.length; i += 3) {
-    if ((gameBoard[i] == player.marker) && (gameBoard[i + 1] == player.marker) && (gameBoard[i + 2] == player.marker
-    )) {
-      return checkWin(player);
-    }
-  }
-
-  // check column
-  for (let i = 0; i < 3; i++) {
-    if ((gameBoard[i] == player.marker) && (gameBoard[i + 3] == player.marker) && (gameBoard[i + 6] ==
-      player.marker)) {
-      return checkWin(player);
-    }
-  }
-
-  // check diagonal
-  if (((gameBoard[0] == player.marker) && (gameBoard[4] == player.marker) && (gameBoard[8] == player.marker)) ||
-    ((gameBoard[2] == player.marker) && (gameBoard[4] == player.marker) && (gameBoard[6] == player.marker))
-  ) {
-    return checkWin(player);
-  }
-
-  return false;
-}
+};
 
 (function () {
 
+  const playerOne = createPlayer("One", 'X');
+  const playerTwo = createPlayer("Two", 'O');
   let currentPlayer = playerOne;
+
+  function checkWin(player) {
+    player.addScore();
+    displayController.playerScoreDisplay(player).innerHTML = `${player.getScore()}`;
+    alert(`Congratulations! Player ${player.name} Won!`);
+    roundReset();
+  };
+
+
+  function checkMatchThree(player) {
+
+      // check row
+      for (let i = 0; i < gameBoard.length; i += 3) {
+        if ((gameBoard[i] == player.marker) && (gameBoard[i + 1] == player.marker) && (gameBoard[i + 2] == player.marker
+        )) {
+          return checkWin(player);
+        }
+      }
+
+      // check column
+      for (let i = 0; i < 3; i++) {
+        if ((gameBoard[i] == player.marker) && (gameBoard[i + 3] == player.marker) && (gameBoard[i + 6] ==
+          player.marker)) {
+          return checkWin(player);
+        }
+      }
+
+      // check diagonal
+      if (((gameBoard[0] == player.marker) && (gameBoard[4] == player.marker) && (gameBoard[8] == player.marker)) ||
+        ((gameBoard[2] == player.marker) && (gameBoard[4] == player.marker) && (gameBoard[6] == player.marker))
+      ) {
+        return checkWin(player);
+      }
+
+      return false;
+  }
+
+
 
   function createGameBoard() {
 
@@ -65,24 +74,34 @@ function checkMatchThree(player) {
       const div = document.createElement(`div`);
       div.classList.add(`grid`);
       gameBoardDisplay.appendChild(div);
-
       (() => {
         function clickGrid() {
-          gameBoard[i] = currentPlayer.marker;
-          div.innerHTML = currentPlayer.marker;
-          currentPlayer = currentPlayer==playerOne ? playerTwo : playerOne;
-          displayController.playerTurnText.innerHTML = `It's Player ${currentPlayer.name}'s turn!`;
-          div.removeEventListener("click", clickGrid);
-        }
 
+          if (!checkMatchThree(currentPlayer)) {
+            gameBoard[i] = currentPlayer.marker;
+            div.innerHTML = currentPlayer.marker;
+
+            checkMatchThree(currentPlayer);
+
+            currentPlayer = currentPlayer == playerOne ? playerTwo : playerOne;
+            displayController.playerTurnText.innerHTML = `It's Player ${currentPlayer.name}'s turn!`;
+            div.removeEventListener("click", clickGrid);
+          }
+        }
         return div.addEventListener("click", clickGrid);
       })();
+    }
+  }
 
-
-      
+  function roundReset() {
+    for (let i = 0; i < gameBoard.length; i++) {
+      gameBoard[i] = '';
+      document.querySelector(`#game-board`).removeChild( document.querySelector(`#game-board`).lastChild );
     }
 
-  }
+    currentPlayer = playerOne;
+    createGameBoard();
+  };
 
   createGameBoard();
 
